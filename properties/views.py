23a54cc15d614d335edf3d1544285938fdf96ced
evans kirimi <evanskirimi483@gmail.com>
2025-08-30 16:21:01 -0700

    created viws to retriev all the properties and maped the view to urls

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+from django.views.decorators.cache import cache_page
+from .models import Property
+
+@cache_page(60 * 15) #cache for 15 minutes(60 seconds * 15)
+def property_list(request):
+    #views to display all the properties cached for 15 minutes
+    properties = Property.objects.all()
+    return render(request, 'properties/property_list.html', {'properties': properties})
