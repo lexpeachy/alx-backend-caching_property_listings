83af535fd1da34c0df703ba5c5badbcfc12e138e
evans kirimi <evanskirimi483@gmail.com>
2025-08-30 16:47:04 -0700

    created signals.py and updated apps.py

--- properties/views.py
+++ properties/views.py
@@ -2,6 +2,8 @@ from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .utils import get_all_properties  # Import the utility function
 import json
+from django.core.cache import cache
+
 
 @cache_page(60 * 15)  # Keep the page-level cache for 15 minutes too
 def property_list(request):
